b4dcc44e01f140d4e44a0845a084f85ee6398723
pcinterpreter <157109152+pcinterpreter@users.noreply.github.com>
2024-01-21 18:35:11 +0500

    Add files via upload

--- interpreter.js
+++ interpreter.js
@@ -1,21 +1,29 @@
 let runButton  = document.getElementById('runbutton');
 let inputArea  = document.getElementById('inputarea');
-let outputArea = document.getElementById('outputArea');
+let outputArea = document.getElementById('outputarea');
 
-const keywords = ['OUTPUT', 'IF', 'THEN', 'ENDIF', 'DECLARE', 'REPEAT', 'WHILE', 'FOR']
+const keywords = ['OUTPUT', 'IF', 'THEN', 'ENDIF', 'DECLARE', 'REPEAT', 'WHILE', 'FOR', 'FUNCTION', 'PROCEDURE', 'TYPE']
+
+// FIX DECIMAL PROBLEM
 
 runButton.addEventListener('click', function(){
-    
-    let programOutput = '';
+    outputArea.value = "";
 
     stack = {
-        ad_name:[],
-        ad_data:[],
+        ad_name:[], // VARIABLE NAME
+        ad_data:[], // VARIABLE DATA
+        ad_daty:[], // VARIABLE DATA TYPE
     }
 
+
+    let dataTypes = {
+        defaultDataTypes: ['INTEGER', 'STRING', 'BOOLEAN', 'REAL', 'CHARACTER'],
+        userDefinedDataTypes: [],
+    }
+    
     
     function tokenizeInput(input){
-        var tokens = input.match(/\b\w+\b|<\-|[^\w\s]/g);
+        var tokens = input.match(/\b\w+\b|<\-|[^\w\s]/g);   
         return tokens;
     }
 
@@ -53,10 +61,15 @@ runButton.addEventListener('click', function(){
                                                 // DETECT WHERE THE OUTPUT BLOCK ENDS AND PUSH THE AXISBLOCK FOR EXECUTION
                     let postOutKeyData = [];
                     let blockEnded     = false;
+                    
 
                     for(let j = i+1; j < code_a.length; j++){
                         for(let k = 0; k < keywords.length; k++){
                             if(code_a[j] == keywords[k]) blockEnded = true;
+                            if(code_a[j+2] == '<-') {
+                                blockEnded = true;
+                                postOutKeyData.push(code_a[j]);
+                            }
                             if(blockEnded) break;
                             
 
@@ -100,6 +113,10 @@ runButton.addEventListener('click', function(){
                 for(let j = i; j < code_a.length; j++){
                     for(let k = 0; k < keywords.length; k++){
                         if(code_a[j] == keywords[k]) blockEnded = true;
+                        if(code_a[j+2] == '<-'){
+                            current_axis_block.push(code_a[j]);
+                            blockEnded = true;
+                        }
                         if(blockEnded) break;
                     }
 
@@ -201,6 +218,66 @@ runButton.addEventListener('click', function(){
                 current_axis_block = [];
             }
 
+            if(code_a[i] == 'PROCEDURE'){
+                let endProcedure = false;
+                
+                for(let j = i; j < code_a.length; j++){
+                    i++;
+                    if(code_a[j] == 'ENDPROCEDURE') endProcedure = true;
+                    current_axis_block.push(code_a[j]);
+                    if(endProcedure) break;
+                }
+
+
+                console.log(current_axis_block);
+                ExecuteCode(current_axis_block);
+                current_axis_block = [];
+            }
+
+            if(code_a[i] == 'FUNCTION'){
+                let endFunction = false;
+                
+                for(let j = i; j < code_a.length; j++){
+                    i++;
+                    if(code_a[j] == 'ENDFUNCTION') endFunction = true;
+                    current_axis_block.push(code_a[j]);
+                    if(endFunction) break;
+                    
+                }
+
+
+                console.log(current_axis_block);
+                ExecuteCode(current_axis_block);
+                current_axis_block = [];
+            }
+
+            if(code_a[i] == 'CALL'){
+                for(let j = i; j < 2; j++){
+                    i++;
+                    current_axis_block.push(code_a[j]);
+                    
+                }
+
+                console.log(current_axis_block);
+                ExecuteCode(current_axis_block);
+                current_axis_block = [];
+            }
+            if(code_a[i] == 'TYPE'){
+                let endType = false;
+                
+                for(let j = i; j < code_a.length; j++){
+                    i++;
+                    if(code_a[j] == 'ENDTYPE') endType = true;
+                    current_axis_block.push(code_a[j]);
+                    if(endType) break;
+                    
+                }
+
+
+                console.log(current_axis_block);
+                ExecuteCode(current_axis_block);
+                current_axis_block = [];
+            }
 
 
             
@@ -211,8 +288,239 @@ runButton.addEventListener('click', function(){
     }
 
     function ExecuteCode(tokens){
+        let programOutput = "";
 
+        if(tokens[0] == "OUTPUT"){
+           if(tokens[1] == '"'){
+                let outStringEnded = false;
+                for(let i = 2; i < tokens.length; i++){
+                    if(tokens[i] == '"') outStringEnded = true;
+                    if(outStringEnded) break;
+                    programOutput += tokens[i]
+                }  
+           }
+           else{ // FOR VARIABLE AND ARITHMATIC
+                let postOutArray = [];
+                for(let i = 1; i < tokens.length; i++){
+                    postOutArray.push(tokens[i]);
+                }
+
+                let variablesFound = findVariables(postOutArray.join(" "));
+
+                if(variablesFound.length == 0){
+                    programOutput += eval(postOutArray.join(' '));
+                }else{
+                    for(let i = 0; i < variablesFound.length; i++){
+                        for(let j = 0; j < postOutArray.length; j++){
+                            if(postOutArray[j] == variablesFound[i]){
+                                
+                                if(fetchVariableType(variablesFound[i], stack) == 'INTEGER'){
+                                    postOutArray[findKeywordInArray(variablesFound[i], postOutArray)] = fetchVarValue(variablesFound[i], stack).toString();
+                                }
+                                else if(fetchVariableType(variablesFound[i], stack) != 'INTEGER' && fetchVariableType(variablesFound[i], stack) != 'STRING' && fetchVariableType(variablesFound[i], stack) != 'REAL'){
+                                    errorHandler(2, variablesFound[i]);
+                                }
+                                
+                            }
+                        }
+                    }
+                    console.log(postOutArray)
+                    programOutput += eval(postOutArray.join(' '));
+                }
+
+                
+            }
+        }
+
+        if(tokens[0] == 'DECLARE'){
+            // DECLARE myVar : INTEGER
+            
+            for(let i = 0; i < stack.ad_data.length; i++){
+                if(tokens[1] == stack.ad_data[i]){
+                    errorHandler(1, tokens[1]);
+                    return;
+                }
+            }
+            
+
+            
+            for(let i = 0; i < dataTypes.defaultDataTypes.length; i++){
+                for(let j = 0; j < dataTypes.userDefinedDataTypes.length; i++){
+                    if(dataTypes.userDefinedDataTypes[j] == tokens[3]){
+                        stack.ad_daty.push(tokens[3]);
+                        stack.ad_data.push('NULL')
+                        stack.ad_name.push(tokens[1])
+                        break;
+                    }
+                    
+                
+                }   
+                
+                
+                if(dataTypes.defaultDataTypes[i] == tokens[3]){
+                    stack.ad_daty.push(tokens[3]);
+                    stack.ad_data.push('NULL')
+                    stack.ad_name.push(tokens[1])
+                    break;
+                }
+
+
+            }
+
+            console.log(stack)
+
+            
+
+            
+        }
+
+
+        if(tokens[1] == '<-'){
+            let variableToAssign = tokens[0];
+            let dataToAssign = [];
+
+            for(let i = 2; i < tokens.length; i++){
+                dataToAssign.push(tokens[i]);
+            }
+
+            let variablesFound = findVariables(dataToAssign.join(' '));
+
+            if(variablesFound.length == 0){
+                console.log(fetchVariableType(variableToAssign, stack));
+                if(fetchVariableType(variableToAssign, stack) == identifyDataType(dataToAssign.join(' '))){
+                    if(fetchVariableType(variableToAssign, stack) == 'INTEGER' || fetchVariableType(variableToAssign, stack) == 'REAL'){
+                        stack.ad_data[fetchVariablePositionInStack(variableToAssign, stack)] = eval(dataToAssign.join(' '));
+                    }else{
+                        stack.ad_data[fetchVariablePositionInStack(variableToAssign, stack)] = dataToAssign.join(' ');
+                    }
+                    
+                }else{
+
+                }
+            }else{
+
+
+                
+                for(let i = 0; i < variablesFound.length; i++){
+                    for(let j = 0; j < dataToAssign.length; j++){
+                        if(dataToAssign[j] == variablesFound[i]){
+                            if(fetchVariableType(variablesFound[i], stack) == fetchVariableType(variableToAssign, stack)){
+                                console.log(true)
+                                dataToAssign[findKeywordInArray(variablesFound[i], dataToAssign)] = fetchVarValue(variablesFound[i], stack);
+                                console.log(dataToAssign)
+
+                            }else{
+                                // ERROR 4
+                            }
+                        }
+                    }
+                }   
+
+                if(identifyDataType(dataToAssign.join(' ')) == 'INTEGER' || identifyDataType(dataToAssign.join(' ')) == 'REAL'){
+                    stack.ad_data[fetchVariablePositionInStack(variableToAssign, stack)] = eval(dataToAssign.join(' '));
+                }else{
+                    stack.ad_data[fetchVariablePositionInStack(variableToAssign, stack)] = dataToAssign.join(' ');
+                }
+                
+            }
+
+            console.log(stack);
+
+        }
+
+        
+
+
+        outputArea.value += programOutput;
+        
     }
+
+
+    function errorHandler(errorCode, furtherArguments){
+        // 1 REDECLARATION - VAR ALREADY EXISTS
+        // 2 VAR DOES NOT MATCH TYPE
+        // 3 VARIABLE VALUE IS NULL
+    }
+
+    function findVariables(expression) {
+        const variableRegex = /[A-Za-z_][A-Za-z0-9_]*/g;
+    
+    
+        const variables = expression.match(variableRegex);
+        if (variables) {
+            const uniqueVariables = [...new Set(variables)];
+            return uniqueVariables;
+        } else {
+            return [];
+        }
+    }
+
+
+    function fetchVarValue(variable, stack){
+        for(let i = 0; i < stack.ad_name.length; i++){
+            if(variable == stack.ad_name[i]){
+                return stack.ad_data[i];
+            }
+        }
+    }
+
+    function findKeywordInArray(keyword, array){
+        for(let i = 0; i < array.length; i++){
+            if(keyword == array[i]){
+                return i;
+            }
+        }
+    }
+
+    function fetchVariableType(variable, stack){
+        for(let i = 0; i < stack.ad_name.length; i++){
+            if(variable == stack.ad_name[i]){
+                return stack.ad_daty[i];
+            }
+        }
+    }
+
+    function fetchVariablePositionInStack(variable, stack){
+        for(let i = 0; i < stack.ad_name.length; i++){
+            if(variable == stack.ad_name[i]){
+                return i;
+            }
+        }
+    }
+
+    function identifyDataType(data) {
+        
+        if (data === 'true' || data === 'false') {
+            return 'BOOLEAN';
+        }
+    
+        
+        try {
+            const result = eval(data);
+    
+            
+            if (!isNaN(result) && result.toString().includes('.')) {
+                return 'REAL';
+            }
+    
+           
+            if (!isNaN(result) && !result.toString().includes('.')) {
+                return 'INTEGER';
+            }
+        } catch (error) {
+            
+        }
+
+        if (data.length === 1) {
+            return 'CHARACTER';
+        }
+    
+    
+        return 'string';
+    }
+    
+    
+    
     
     codeSorter(tokenizeInput(inputArea.value));
 })
--- test.js
+++ test.js
@@ -1,6 +1,1 @@
-const array1 = [1, 2, 3];
-const array2 = [4, 5, 6];
-
-const mergedArray = [...array1];
-mergedArray.push(...array2);
-console.log(mergedArray); // Output: [1, 2, 3, 4, 5, 6]
+console.log(eval('1'))
